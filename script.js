47744284ad34d02581c522809cf070304381c7f8
Rishi <rishijain0612@gmail.com>
2025-01-21 15:12:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function toggleImageBox() {
+    var box = document.querySelector('.image-box');
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
+
+
+
+ 
